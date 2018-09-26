fe1607ff4d1c17b3cf4b3f688fb5b6a748f6aa9a
ajazz <ajazzbegins@gmail.com>
2018-09-26 12:36:02 +0530

    added AccountSearch and modified HomePage

---
+++ src/main/java/com/sample/qa/pages/AccountSearch.java
@@ -0,0 +1,10 @@
+package com.sample.qa.pages;
+
+public class AccountSearch {
+	
+public void AccountSearch() {
+	System.out.println("AccountSearch Method is executed");
+	
+}
+
+}
--- src/main/java/com/sample/qa/pages/HomePage.java
+++ src/main/java/com/sample/qa/pages/HomePage.java
@@ -1,5 +1,9 @@
 package com.sample.qa.pages;
 
 public class HomePage {
+	
+	public void testMethods(){
+		System.out.println("test method inside HomePage class");
+	}
 
 }
